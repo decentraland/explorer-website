936cce3e24385eb32976410fd202abedddb61f67
Agustin Mendez <github@menduz.com>
2021-07-09 13:07:06 -0300

    fix: versions in paths and rollbar

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,27 @@
 module.exports = function (app) {
+  app.use("/cdn/packages/website/:version/*", (req, res, next) => {
+    const options = {
+      root: `./public/${req.params.package}`,
+      dotfiles: "deny",
+      headers: {
+        "x-timestamp": Date.now(),
+        "x-sent": true,
+      },
+    };
+
+    const fileName = req.params[0];
+
+    console.log("fileName", fileName);
+
+    res.sendFile(fileName, options, (err) => {
+      if (err) {
+        next(err);
+      } else {
+        console.log("Sent:", fileName);
+      }
+    });
+  });
+
   app.use("/cdn/packages/:package/:version/*", (req, res, next) => {
     const options = {
       root: `./node_modules/${req.params.package}`,
