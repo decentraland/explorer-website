8b71995de54d15e63a076a7d5e23cc38580ce11d
Agustin Mendez <github@menduz.com>
2021-07-19 17:18:48 -0300

    format

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,47 +1,47 @@
 module.exports = function (app) {
-  app.use("/cdn/packages/website/:version/*", (req, res, next) => {
+  app.use('/cdn/packages/website/:version/*', (req, res, next) => {
     const options = {
       root: `./public/${req.params.package}`,
-      dotfiles: "deny",
+      dotfiles: 'deny',
       headers: {
-        "x-timestamp": Date.now(),
-        "x-sent": true,
-      },
-    };
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    }
 
-    const fileName = req.params[0];
+    const fileName = req.params[0]
 
-    console.log("fileName", fileName);
+    console.log('fileName', fileName)
 
     res.sendFile(fileName, options, (err) => {
       if (err) {
-        next(err);
+        next(err)
       } else {
-        console.log("Sent:", fileName);
+        console.log('Sent:', fileName)
       }
-    });
-  });
+    })
+  })
 
-  app.use("/cdn/packages/:package/:version/*", (req, res, next) => {
+  app.use('/cdn/packages/:package/:version/*', (req, res, next) => {
     const options = {
       root: `./node_modules/${req.params.package}`,
-      dotfiles: "deny",
+      dotfiles: 'deny',
       headers: {
-        "x-timestamp": Date.now(),
-        "x-sent": true,
-      },
-    };
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    }
 
-    const fileName = req.params[0];
+    const fileName = req.params[0]
 
-    console.log("fileName", fileName);
+    console.log('fileName', fileName)
 
     res.sendFile(fileName, options, (err) => {
       if (err) {
-        next(err);
+        next(err)
       } else {
-        console.log("Sent:", fileName);
+        console.log('Sent:', fileName)
       }
-    });
-  });
-};
+    })
+  })
+}
