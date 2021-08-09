ecee803961c3347d3b92234eb8d90fc2f9aa41c1
Santi Andrade <sandrade@decentraland.org>
2021-08-09 12:25:54 +0200

    Fix compilation error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -64,7 +64,7 @@ export const recommendedBrowsers: string[] = [
 
 export function isRecommendedBrowser() {
   for (let i = 0; i < recommendedBrowsers.length; i++) {
-    if (navigator.userAgent.indexOf(recommendedBrowsers[i]) != -1) {
+    if (navigator.userAgent.indexOf(recommendedBrowsers[i]) !== -1) {
       return true
     }
   }
