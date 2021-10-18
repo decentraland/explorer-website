fbebd8ce98811160dc271a90a140bf1f1f6cd1b0
frami <i@2fd.me>
2021-10-18 14:49:52 -0500

    fix: loading image from cdn

--- src/components/common/Loading/LoadingRender.tsx
+++ src/components/common/Loading/LoadingRender.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import loading from '../../../images/loading.gif'
 import './LoadingRender.css'
 
 export const LoadingRender = React.memo(function () {
   return <div className="LoadingRender">
-    <img alt="loading" src="./images/loading.gif" width="150" height="150" />
+    <img alt="loading" src={process.env.PUBLIC_URL + '/images/loading.gif'} width="150" height="150" />
     <p>We are downloading the latest version of Decentraland. You'll be up and running in a few seconds!</p>
   </div>
 })
