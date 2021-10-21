064fe18509d607aa6c6c11a4ee181aa299d90902
frami <i@2fd.me>
2021-10-21 06:56:53 -0500

    fix: copies

--- src/components/banners/BannerStream.tsx
+++ src/components/banners/BannerStream.tsx
@@ -7,9 +7,9 @@ export const BannerStream: React.FC<Pick<BannerContainerProps, 'onClose'>> = (pr
   <div id="banner-stream" className="banner-container">
     <div className="banner-close-button" onClick={props.onClose} />
     <div className="banner-text">
-      You are seeing a stream. The client is only available on desktop.
+        You are viewing a stream because Decentraland is only available on desktop.
         <br />
-        <a href="https://decentraland.org" rel="noreferrer noopener" target="_blank">Learn more about decentraland</a>
+        <a href="https://decentraland.org" rel="noreferrer noopener" target="_blank">Learn more</a>
     </div>
   </div>
 )
