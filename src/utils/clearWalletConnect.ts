dad4ee49c73d45a4f6724fbad6381826dc7a396e
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-07 11:54:32 -0300

    chore: Add more comments

--- src/utils/clearWalletConnect.ts
+++ src/utils/clearWalletConnect.ts
@@ -5,10 +5,13 @@ const WC_STORAGE_KEY = 'walletconnect'
 
 export function clearWalletConnect() {
   try {
-    // Remove data created and stored by wallet connect which tracks the wallet that has connected before
+    // Remove data created and stored by wallet connect which tracks the wallet that has connected before.
+    // Removing this will show the QR code to connect even if the user has the wallet already connected.
     localStorage.removeItem(WC_STORAGE_KEY)
 
-    // Remove dcl connect stored data only if the provider is wallet connect
+    // Remove dcl connect stored data only if the provider is wallet connect.
+    // Removing this will prevent showing the QR code automatically on init if the user connected to the app
+    // with wallet connect previously.
     const connectData = localStorage.getItem(DCL_CONNECT_STORAGE_KEY)
 
     if (connectData) {
