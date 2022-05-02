2c2cf3c798894c223d5b619ead8cd4f1e043f7bc
menduz <github@menduz.com>
2022-05-02 18:35:45 -0300

    chore: update versions (#174)
    * update versions
    
    * npm cleanup
    
    * tests were not working
    
    * chore: update @dcl/schemas
    
    Co-authored-by: frami <i@2fd.me>

--- src/components/auth/LoginContainer.stories.tsx
+++ src/components/auth/LoginContainer.stories.tsx
@@ -2,26 +2,26 @@ import React from 'react'
 import { Meta, Story } from '@storybook/react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { LoginContainer, LoginContainerProps, LoginStage } from './LoginContainer'
+import { LoginContainer, LoginContainerProps } from './LoginContainer'
 
-export default {
-  title: 'Explorer/Login',
-  args: {
-    stage: LoginStage.LOADING,
-    subStage: ''
-  } as LoginContainerProps,
-  component: LoginContainer,
-  argTypes: {
-    onLogin: { action: 'signing in...' },
-    onTermsChange: { action: 'terms changed...' }
-  }
-} as Meta
+// export default {
+//   title: 'Explorer/Login',
+//   args: {
+//     stage: LoginStage.LOADING,
+//     subStage: ''
+//   } as LoginContainerProps,
+//   component: LoginContainer,
+//   argTypes: {
+//     onLogin: { action: 'signing in...' },
+//     onTermsChange: { action: 'terms changed...' }
+//   }
+// } as Meta
 
-const Template: Story<LoginContainerProps & { state: any }> = ({ state, ...props }) => (
-  <Provider store={createStore(() => state, state)}>
-    <LoginContainer {...props} />
-  </Provider>
-)
+// const Template: Story<LoginContainerProps & { state: any }> = ({ state, ...props }) => (
+//   <Provider store={createStore(() => state, state)}>
+//     <LoginContainer {...props} />
+//   </Provider>
+// )
 
 // export const LoadingState = Template.bind({});
 // LoadingState.args = {
@@ -40,14 +40,14 @@ const Template: Story<LoginContainerProps & { state: any }> = ({ state, ...props
 //   stage: LoginStage.SIGN_IN,
 // };
 
-export const ConnectAdvice = Template.bind({})
-ConnectAdvice.args = {
-  ...Template.args,
-  stage: LoginStage.CONNECT_ADVICE
-}
+// export const ConnectAdvice = Template.bind({})
+// ConnectAdvice.args = {
+//   ...Template.args,
+//   stage: LoginStage.CONNECT_ADVICE
+// }
 
-export const SignAdvice = Template.bind({})
-SignAdvice.args = {
-  ...Template.args,
-  stage: LoginStage.SIGN_ADVICE
-}
+// export const SignAdvice = Template.bind({})
+// SignAdvice.args = {
+//   ...Template.args,
+//   stage: LoginStage.SIGN_ADVICE
+// }
