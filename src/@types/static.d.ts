56c68036dda972ac51e94b1ae057c63b58c00e85
frami <i@2fd.me>
2023-01-17 13:21:12 -0300

    fix: add types for static files

---
+++ src/@types/static.d.ts
@@ -0,0 +1,15 @@
+declare module '*.mp4' {
+  export default string;
+}
+declare module '*.mp3' {
+  export default string;
+}
+declare module '*.png' {
+  export default string;
+}
+declare module '*.jpg' {
+  export default string;
+}
+declare module '*.gif' {
+  export default string;
+}
