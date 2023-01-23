d16af9b4cdaea5ff558efee867ec264268a897ba
frami <i@2fd.me>
2023-01-23 12:11:41 -0600

    fix: reduce clutter (#333)
    * fix: add types for static files
    
    * fix: replace loading gif with a video
    
    * lint: remove warnings: Unnecessary escape character: \"
    
    * fix: remove corrupted file
    
    * fix: remove background preload
    
    * fix: remove unused files
    
    * fix: remove unused import
    
    * chore: update snapshot
    
    * fix: remove preloading unused gif
    
    * fix: add link[rel="preconnect"] to thegraph

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { useMobileMediaQuery } from 'decentraland-ui/dist/components/Media'
 import ErrorContainer from './errors/ErrorContainer'
 import LoginContainer from './auth/LoginContainer'
-import { Audio } from './common/Audio'
 import { StoreType } from '../state/redux'
 import { isElectron } from '../integration/desktop'
 import { BeginnersGuide } from './auth/BeginnersGuide'
@@ -13,6 +12,7 @@ import { LoadingRender } from './common/Loading/LoadingRender'
 import { Navbar } from './common/Layout/Navbar'
 import { FeatureFlags, getFeatureVariant } from '../state/selectors'
 import StreamContainer from './common/StreamContainer'
+import { Audio } from './common/Audio'
 import { isMobile } from '../integration/browser'
 import MobileContainer from './common/MobileContainer'
 import CatalystWarningContainer from './warning/CatalystWarningContainer'
@@ -70,8 +70,13 @@ const App: React.FC<AppProps> = (props) => {
 
   return (
     <div className={`WebsiteApp ${props.hasBanner ? 'withBanner' : ''}`}>
-      {!isElectron() && props.sound && <Audio track={`${process.env.PUBLIC_URL}/tone4.mp3`} play />}
       <BannerContainer />
+      {/**
+        * The audio tag is required to prevent the tab from halting the scripting if left unfocused
+        * @see https://github.com/decentraland/explorer-website/pull/333#discussion_r1084094994
+        * @see https://github.com/eordano/background-throttle
+        */}
+      {!isElectron() && props.sound && <Audio track={`${process.env.PUBLIC_URL}/tone4.mp3`} play />}
       {!isElectron() && <Navbar /> }
       <LoginContainer />
       {!isElectron() && <BeginnersGuide /> }
