766c398f0a46b7cc6da3f0b3ca0e411f9be7e206
frami <i@2fd.me>
2023-01-23 14:55:30 -0300

    fix: restore audio file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,6 +12,7 @@ import { LoadingRender } from './common/Loading/LoadingRender'
 import { Navbar } from './common/Layout/Navbar'
 import { FeatureFlags, getFeatureVariant } from '../state/selectors'
 import StreamContainer from './common/StreamContainer'
+import { Audio } from './common/Audio'
 import { isMobile } from '../integration/browser'
 import MobileContainer from './common/MobileContainer'
 import CatalystWarningContainer from './warning/CatalystWarningContainer'
@@ -70,6 +71,12 @@ const App: React.FC<AppProps> = (props) => {
   return (
     <div className={`WebsiteApp ${props.hasBanner ? 'withBanner' : ''}`}>
       <BannerContainer />
+      {/**
+        * The audio tag is required to prevent the tab from halting the scripting if left unfocused
+        * @see https://github.com/decentraland/explorer-website/pull/333#discussion_r1084094994
+        * @see https://github.com/eordano/background-throttle
+        */}
+      {!isElectron() && props.sound && <Audio track={`${process.env.PUBLIC_URL}/tone4.mp3`} play />}
       {!isElectron() && <Navbar /> }
       <LoginContainer />
       {!isElectron() && <BeginnersGuide /> }
