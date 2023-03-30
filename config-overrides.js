bc4f2ab7f5f89f55cd7694e291f49a455a48d153
Nando <24811313+fzavalia@users.noreply.github.com>
2023-03-30 16:54:54 -0300

    feat: Add customize cra lockfile

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+// config-overrides.js
+
+const { override, babelInclude } = require('customize-cra')
+const path = require('path')
+
+module.exports = override(
+  babelInclude([
+    path.resolve(__dirname, 'src'),
+    path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect'),
+    path.resolve(__dirname, 'node_modules/@walletconnect'),
+    path.resolve(__dirname, 'node_modules/@web3modal')
+  ])
+)
