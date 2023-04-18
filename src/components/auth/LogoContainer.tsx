d5fd8b9e99675774048a07d8afb223f8369614eb
frami <i@2fd.me>
2023-04-18 17:35:06 -0300

    refactor: move LogoContainer to an stateless component

--- src/components/auth/LogoContainer.tsx
+++ src/components/auth/LogoContainer.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import { StoreType } from '../../state/redux'
 import logoWhite from '../../images/logo-white.svg'
-import { FeatureFlags, getFeatureVariantName, VariantNames } from '../../state/selectors'
-import { isElectron } from '../../integration/desktop'
-import { connect } from 'react-redux'
 import './LogoContainer.css'
 
-
-const mapStateToProps = (state: StoreType): LogoContainerProps => {
-  return {}
-}
-
-export interface LogoContainerProps {}
-
-export const LogoContainer: React.FC<LogoContainerProps> = ({}) => {
+export default React.memo(function LogoContainer() {
   return (
     <div className="LogoContainer">
       <img alt="decentraland" src={logoWhite} height="40" width="212" />
       <p>Sign In or Create an Account</p>
     </div>
   )
-}
-
-export default connect(mapStateToProps)(LogoContainer)
+})
