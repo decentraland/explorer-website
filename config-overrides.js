b1844eff05dec3eb36fb216c7f7fa2c1c4c6f0f5
Nando <24811313+fzavalia@users.noreply.github.com>
2023-04-19 00:11:28 -0300

    feat: Update overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,21 @@
-// config-overrides.js
-
 const { override, babelInclude } = require('customize-cra')
 const path = require('path')
 
-module.exports = override(
-  babelInclude([
-    path.resolve(__dirname, 'src'),
-    path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect'),
-    path.resolve(__dirname, 'node_modules/@walletconnect'),
-    path.resolve(__dirname, 'node_modules/@web3modal')
-  ])
-)
+/**
+ * WalletConnectV2 is implemented by importing @walletconnect/ethereum-provider, which is a dependency of decentraland-connect.
+ * This library has some code that is not supported by current version of react-scripts used by this project.
+ * To be able to use it. We can add the files with unsupported code to the babelInclude list.
+ */
+const wcEthereumProviderSupport = [
+  path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect/ethereum-provider/dist/index.es.js'),
+  path.resolve(__dirname, 'node_modules/@walletconnect/universal-provider/dist/index.es.js'),
+  path.resolve(__dirname, 'node_modules/@walletconnect/universal-provider/node_modules/@walletconnect/utils/dist/index.es.js'),
+  path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect/utils/dist/index.es.js'),
+  path.resolve(__dirname, 'node_modules/@walletconnect/sign-client/dist/index.es.js'),
+  path.resolve(__dirname, 'node_modules/@walletconnect/sign-client/node_modules/@walletconnect/utils/dist/index.es.js'),
+  path.resolve(__dirname, 'node_modules/@web3modal/core/dist/index.js'),
+  path.resolve(__dirname, 'node_modules/@web3modal/ui/dist/index.js'),
+  path.resolve(__dirname, 'node_modules/@walletconnect/sign-client/node_modules/@walletconnect/core/dist/index.es.js')
+]
+
+module.exports = override(babelInclude([path.resolve(__dirname, 'src'), ...wcEthereumProviderSupport]))
