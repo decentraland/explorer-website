57828ab560ef8b825ca52fa4b73cb80c697b2e3a
frami <i@2fd.me>
2023-05-24 09:59:40 -0300

    fix: use default leftMenu

--- src/components/common/Layout/Navbar.tsx
+++ src/components/common/Layout/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Navbar as Base } from'decentraland-ui/dist/components/Navbar/Navbar'
 import { JoinDiscord } from '../Button/JoinDiscord'
-import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu'
 
 export type NavbarProps = {
   rightMenu?: React.ReactNode
@@ -10,35 +9,6 @@ export type NavbarProps = {
 export const Navbar = React.memo(function (props: NavbarProps) {
   return <Base
     isFullscreen
-    leftMenu={<>
-        <Menu.Item href="https://decentraland.org">
-          HOME
-        </Menu.Item>
-        <Menu.Item active href="https://play.decentraland.org" onClick={(e) => e.preventDefault()}>
-          PLAY
-        </Menu.Item>
-       <Menu.Item href="https://market.decentraland.org">
-          {Base.defaultProps.i18n?.menu?.marketplace}
-        </Menu.Item>
-        <Menu.Item href="https://builder.decentraland.org">
-          {Base.defaultProps.i18n?.menu?.builder}
-        </Menu.Item>
-        <Menu.Item href="https://docs.decentraland.org">
-          {Base.defaultProps.i18n?.menu?.docs}
-        </Menu.Item>
-        <Menu.Item href="https://places.decentraland.org">
-          {Base.defaultProps.i18n?.menu?.places}
-        </Menu.Item>
-        <Menu.Item href="https://events.decentraland.org">
-          {Base.defaultProps.i18n?.menu?.events}
-        </Menu.Item>
-        <Menu.Item href="https://dao.decentraland.org">
-          {Base.defaultProps.i18n?.menu?.dao}
-        </Menu.Item>
-        <Menu.Item href="https://decentraland.org/blog">
-          {Base.defaultProps.i18n?.menu?.blog}
-        </Menu.Item>
-    </>}
     rightMenu={props.rightMenu ?? <JoinDiscord />}
   />
 })
