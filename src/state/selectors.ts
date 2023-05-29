a6738e946c72cd03c62fb8d768d6ea75a4a0507d
frami <i@2fd.me>
2023-05-29 14:47:19 -0300

    refactor: pick from state

--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -24,13 +24,13 @@ export enum ABTestingVariant {
   Disabled = 'disabled',
 }
 
-export function isFeatureEnabled(state: StoreType, key: string): boolean {
+export function isFeatureEnabled(state: Pick<StoreType, 'featureFlags'>, key: string): boolean {
   const name = `${FF_APPLICATION_NAME}-${key}`
   const ff = state.featureFlags || defaultFeatureFlagsState
   return !!ff.flags[name]
 }
 
-export function getFeatureVariantValue(state: StoreType, key: string, defaultValue: string | undefined = undefined) {
+export function getFeatureVariantValue(state: Pick<StoreType, 'featureFlags'>, key: string, defaultValue: string | undefined = undefined) {
   if (isFeatureEnabled(state, key)) {
     const name = `${FF_APPLICATION_NAME}-${key}`
     const variant = state.featureFlags.variants[name]
@@ -42,7 +42,7 @@ export function getFeatureVariantValue(state: StoreType, key: string, defaultVal
   return defaultValue
 }
 
-export function getFeatureVariantName(state: StoreType, key: string, defaultValue?: string) {
+export function getFeatureVariantName(state: Pick<StoreType, 'featureFlags'>, key: string, defaultValue?: string) {
   if (isFeatureEnabled(state, key)) {
     const name = `${FF_APPLICATION_NAME}-${key}`
     const variant = state.featureFlags.variants[name]
@@ -54,15 +54,15 @@ export function getFeatureVariantName(state: StoreType, key: string, defaultValu
   return defaultValue
 }
 
-export function isFeatureVariantEnabled(state: StoreType, key: string) {
+export function isFeatureVariantEnabled(state: Pick<StoreType, 'featureFlags'>, key: string) {
   const variant = getFeatureVariantName(state, key, ABTestingVariant.Disabled)
   return  variant === ABTestingVariant.Enabled
 }
 
-export function isWaitingForRenderer(state: StoreType): boolean {
+export function isWaitingForRenderer(state: Pick<StoreType, 'session'>): boolean {
   return state.session?.kernelState?.loginStatus === LoginState.WAITING_RENDERER
 }
 
-export function isLoginComplete(state: StoreType): boolean {
+export function isLoginComplete(state: Pick<StoreType, 'session'>): boolean {
   return state.session?.kernelState?.loginStatus === LoginState.COMPLETED
 }
