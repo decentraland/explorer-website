4dbbc591a90cf301bd872262a648fb48c4f06e74
Nando <24811313+fzavalia@users.noreply.github.com>
2023-06-08 12:36:35 -0300

    fix: Only override in dev

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,17 @@
 const { override, babelInclude } = require('customize-cra')
 const path = require('path')
 
-module.exports = override(
-  babelInclude([
-    path.resolve(__dirname, 'src'),
-    path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect'),
-    path.resolve(__dirname, 'node_modules/@walletconnect'),
-    path.resolve(__dirname, 'node_modules/@web3modal')
-  ])
-)
+let overrideArgs = []
+
+if (process.env.NODE_ENV === 'development') {
+  overrideArgs = [
+    babelInclude([
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect'),
+      path.resolve(__dirname, 'node_modules/@walletconnect'),
+      path.resolve(__dirname, 'node_modules/@web3modal')
+    ])
+  ]
+}
+
+module.exports = override(overrideArgs)
