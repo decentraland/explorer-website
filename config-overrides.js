39ff96c77de4ddaf276534f7ca3fa1d1ab3c8d08
Nando <24811313+fzavalia@users.noreply.github.com>
2023-06-08 12:39:10 -0300

    fix: Add comments

--- config-overrides.js
+++ config-overrides.js
@@ -5,8 +5,11 @@ let overrideArgs = []
 
 if (process.env.NODE_ENV === 'development') {
   overrideArgs = [
+    // Overwrites the include option for babel loader, for when you need to transpile a module in your node_modules folder.
     babelInclude([
+      // Src has to be included or else your own app won't be transpiled.
       path.resolve(__dirname, 'src'),
+      // WalletConnectV2 modules that need to be transpiled.
       path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect'),
       path.resolve(__dirname, 'node_modules/@walletconnect'),
       path.resolve(__dirname, 'node_modules/@web3modal')
