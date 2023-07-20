80ad80a8d0d890f7af0c9773ad24d2820085b5f5
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2023-07-20 15:12:00 -0300

    fix: Show proper message when connecting with WC (#404)

--- src/components/auth/EthConnectAdvice.tsx
+++ src/components/auth/EthConnectAdvice.tsx
@@ -9,19 +9,25 @@ export type EthWalletSelectorAdviceProps = {
 export const EthConnectAdvice = React.memo((props: EthWalletSelectorAdviceProps) => {
   switch (props.provider) {
     case ProviderType.FORTMATIC:
-      return <span className="EthConnectAdvice" style={props.style}>
-        Please, follow the instructions of your fortmatic provider to continue.
-      </span>
+      return (
+        <span className="EthConnectAdvice" style={props.style}>
+          Please, follow the instructions of your fortmatic provider to continue.
+        </span>
+      )
 
     case ProviderType.INJECTED:
-      return <span className="EthConnectAdvice" style={props.style}>
-        Please, follow the instructions of your browser wallet provider to continue.
-      </span>
+      return (
+        <span className="EthConnectAdvice" style={props.style}>
+          Please, follow the instructions of your browser wallet provider to continue.
+        </span>
+      )
 
-    case ProviderType.WALLET_CONNECT:
-      return <span className="EthConnectAdvice" style={props.style}>
-        Please, follow the instructions of your mobile wallet provider to continue.
-      </span>
+    case ProviderType.WALLET_CONNECT_V2:
+      return (
+        <span className="EthConnectAdvice" style={props.style}>
+          Please follow the instructions from your wallet provider's app on your mobile device to continue.
+        </span>
+      )
 
     default:
       return null
