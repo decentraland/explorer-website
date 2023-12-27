91b1f7afce8433d179b5afef5a7856438ad046a3
Nando <24811313+fzavalia@users.noreply.github.com>
2023-12-27 00:28:20 -0300

    feat: Add sign in step

--- src/components/auth/LoginWithAuthServerPage.tsx
+++ src/components/auth/LoginWithAuthServerPage.tsx
@@ -1,18 +1,74 @@
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import './LoginWithAuthServerPage.css'
+import { AuthServerProvider } from 'decentraland-connect'
+import { useRef, useState } from 'react'
+
+enum View {
+  WELCOME,
+  SIGN_IN_CODE
+}
 
 export const LoginWithAuthServerPage = () => {
-  return (
-    <div className="LoginWithAuthServerPage">
-      <div className="background"></div>
-      <div className="main">
-        <div className="logo"></div>
-        <div className="title">Unlock Your Virtual World.</div>
-        <div className="subtitle">Access and start exploring.</div>
-        <Button className="start" primary>
-          Start
-        </Button>
+  const [view, setView] = useState<View>(View.WELCOME)
+  const initSignInResultRef = useRef<any>()
+
+  if (view === View.WELCOME) {
+    return (
+      <div className="LoginWithAuthServerPage">
+        <div className="background"></div>
+        <div className="main">
+          <div className="logo"></div>
+          <div className="title">Unlock Your Virtual World.</div>
+          <div className="subtitle">Access and start exploring.</div>
+          <Button
+            className="button"
+            primary
+            onClick={async () => {
+              const initSignInResult = await AuthServerProvider.initSignIn()
+              initSignInResultRef.current = initSignInResult
+              setView(View.SIGN_IN_CODE)
+            }}
+          >
+            Start
+          </Button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  if (view === View.SIGN_IN_CODE) {
+    return (
+      <div className="LoginWithAuthServerPage">
+        <div className="background"></div>
+        <div className="main">
+          <div
+            className="back"
+            onClick={() => {
+              initSignInResultRef.current = null
+              setView(View.WELCOME)
+            }}
+          ></div>
+          <div className="title">Secure sign-in step</div>
+          <div className="subtitle-sign-in-code">
+            Remember the verification number below. You'll be prompted to confirm it in your web browser to securely
+            link your sign in.
+          </div>
+          <div className="code">{initSignInResultRef.current!.requestResponse.code}</div>
+          <Button
+            className="button"
+            primary
+            onClick={async () => {
+              const initSignInResult = await AuthServerProvider.initSignIn()
+              initSignInResultRef.current = initSignInResult
+              setView(View.SIGN_IN_CODE)
+            }}
+          >
+            Continue to sign in
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  return <div>asd</div>
 }
