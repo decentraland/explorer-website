a1e7551392ac9e170407e812117d9afd82f19b8d
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-01-03 12:51:06 -0300

    Update src/state/sagas.ts
    Co-authored-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
    Signed-off-by: Melisa Anabella Rossi <merossi@itba.edu.ar>

--- src/state/sagas.ts
+++ src/state/sagas.ts
@@ -4,7 +4,7 @@ import { createProfileSaga } from 'decentraland-dapps/dist/modules/profile/sagas
 import { getWantedChainId } from '../kernel-loader'
 
 const PEER_URL = window.location.origin.includes('decentraland.org')
-  ? 'https://perr.decentraland.org'
+  ? 'https://peer.decentraland.org'
   : 'https://peer.decentraland.zone'
 
 const baseProfileSaga = createProfileSaga({ peerUrl: PEER_URL, getIdentity: () => undefined })
