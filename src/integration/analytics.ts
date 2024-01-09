abd96f91c81a8b466461d2b9f1b02432f7d3b722
Agnieszka Besz <aga@besz.me>
2024-01-09 15:23:54 +0000

    Add test log

--- src/integration/analytics.ts
+++ src/integration/analytics.ts
@@ -33,6 +33,7 @@ export const defaultAnalyticsOptions: AnalyticsOptions = {
 
 // TODO fill with segment keys and integrate identity server
 export function configureSegment() {
+  console.info('explorer-website: DEBUG_ANALYTICS', 'Host:', globalThis.location.host, 'Path:', globalThis.location.pathname);
   // all decentraland.org domains are considered PRD
   if (globalThis.location.host.endsWith('.decentraland.org')) {
     return initialize(AnalyticsAccount.PRD)
