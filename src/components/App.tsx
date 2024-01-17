4ea74b5035b2227c403fc172b3f97283c7a73283
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2024-01-17 17:25:08 -0300

    feat: Remove feature flags (#452)
    * feat: Remove feature flags
    
    * fix: Remove unused images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,16 +15,10 @@ import {
   isFeatureEnabled
 } from '../state/selectors'
 import { isMobile } from '../integration/browser'
-import { BeginnersGuide } from './auth/BeginnersGuide'
-import { BigFooter } from './common/Layout/BigFooter'
-import { Navbar } from './common/Layout/Navbar'
-import BannerContainer from './banners/BannerContainer'
 import { LoadingRender } from './common/Loading/LoadingRender'
 import { initializeKernel } from '../integration/kernel'
 import StreamContainer from './common/StreamContainer'
 import ErrorContainer from './errors/ErrorContainer'
-import LoginContainer from './auth/LoginContainer'
-import { Audio } from './common/Audio'
 import Start from './start'
 import MobileContainer from './common/MobileContainer'
 import CatalystWarningContainer from './warning/CatalystWarningContainer'
@@ -46,7 +40,6 @@ function mapStateToProps(state: StoreType): AppProps {
   const trustedCatalyst = !!state.catalyst?.trusted
   const error = !!state.error.error
   const sound = true // TODO: sound must be true after the first click
-  const isDesktopClientSignInWithAuthDappEnabled = isFeatureEnabled(state, FeatureFlags.DesktopClientSignInWithAuthDapp)
 
   return {
     seamlessLogin,
@@ -59,9 +52,7 @@ function mapStateToProps(state: StoreType): AppProps {
     trustedCatalyst,
     error,
     sound,
-    featureFlagsLoaded: !!state.featureFlags.ready,
-    isAuthDappEnabled: isFeatureEnabled(state, FeatureFlags.AuthDapp),
-    isDesktopClientSignInWithAuthDappEnabled
+    featureFlagsLoaded: !!state.featureFlags.ready
   }
 }
 
@@ -76,9 +67,7 @@ export interface AppProps {
   trustedCatalyst: boolean
   error: boolean
   sound: boolean
-  isAuthDappEnabled: boolean
   featureFlagsLoaded: boolean
-  isDesktopClientSignInWithAuthDappEnabled: boolean
 }
 
 const App: React.FC<AppProps> = (props) => {
@@ -86,10 +75,10 @@ const App: React.FC<AppProps> = (props) => {
   const small = useMobileMediaQuery()
 
   useEffect(() => {
-    if (isElectron() || (props.featureFlagsLoaded && !props.isAuthDappEnabled)) {
+    if (isElectron()) {
       initializeKernel()
     }
-  }, [props.isAuthDappEnabled, props.featureFlagsLoaded])
+  }, [])
 
   if (!props.trustedCatalyst) {
     return <CatalystWarningContainer />
@@ -125,33 +114,15 @@ const App: React.FC<AppProps> = (props) => {
     return <LoadingRender />
   }
 
-  if (props.isAuthDappEnabled && !isElectron()) {
+  if (!isElectron()) {
     return (
       <WalletProvider>
         <Start initializeKernel={initializeKernel} />
       </WalletProvider>
     )
-  }
-
-  if (isElectron() && props.isDesktopClientSignInWithAuthDappEnabled) {
+  } else {
     return <LoginWithAuthServerPage />
   }
-
-  return (
-    <div className={`WebsiteApp ${props.hasBanner ? 'withBanner' : ''}`}>
-      <BannerContainer />
-      {/**
-       * The audio tag is required to prevent the tab from halting the scripting if left unfocused
-       * @see https://github.com/decentraland/explorer-website/pull/333#discussion_r1084094994
-       * @see https://github.com/eordano/background-throttle
-       */}
-      {!isElectron() && props.sound && <Audio track={`${(import.meta as any).env.VITE_PUBLIC_URL}/tone4.mp3`} play />}
-      {!isElectron() && <Navbar />}
-      <LoginContainer />
-      {!isElectron() && <BeginnersGuide />}
-      {!isElectron() && <BigFooter />}
-    </div>
-  )
 }
 
 export default connect(mapStateToProps)(App)
