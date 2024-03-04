fc4b4cf6cd2e179a3d0ec1868fed9c355ab8f519
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2024-03-04 12:17:07 -0300

    fix: Use 3D platform for avatar preview (#456)
    * fix: Remove platform
    
    * fix: Add platform wearable definition
    
    * Add custom wearable preview
    
    * fix: Integrate new platform
    
    * fix: Update platform
    
    * fix: Update platform definition
    
    * fix: Remove platform definition
    
    In favor of using base64 so the url of the glb can be determined by the current page origin.
    
    * fix: Small change
    
    * fix: Update styles

---
+++ src/components/common/CustomWearablePreview/CustomWearablePreview.tsx
@@ -0,0 +1,56 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { PreviewEmote } from '@dcl/schemas'
+import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+import { WearablePreview } from 'decentraland-ui/dist/components/WearablePreview/WearablePreview'
+import { Props } from './CustomWearablePreview.types'
+import './CustomWearablePreview.css'
+
+export const CustomWearablePreview = (props: Props) => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => setIsLoading(true), [props.profile])
+
+  const platformDefinition = useMemo(() => {
+    const getRepresentation = (bodyShape: 'BaseMale' | 'BaseFemale') => {
+      const mainFile = 'platform.glb'
+      return {
+        bodyShapes: [`urn:decentraland:off-chain:base-avatars:${bodyShape}`],
+        mainFile,
+        contents: [
+          {
+            key: mainFile,
+            url: `${window.location.origin}/misc/platform/${mainFile}`
+          }
+        ]
+      }
+    }
+
+    return btoa(
+      JSON.stringify({
+        data: {
+          representations: [getRepresentation('BaseMale'), getRepresentation('BaseFemale')]
+        }
+      })
+    )
+  }, [])
+
+  const handleOnLoad = useCallback(() => setIsLoading(false), [])
+
+  return (
+    <div className="CustomWearablePreview">
+      <WearablePreview
+        lockBeta={true}
+        panning={false}
+        disableBackground={true}
+        profile={props.profile}
+        dev={false}
+        emote={PreviewEmote.WAVE}
+        disableAutoRotate
+        cameraY={0.2}
+        base64s={[platformDefinition]}
+        onLoad={handleOnLoad}
+      />
+      {isLoading ? <Loader active={true} size="huge" /> : null}
+    </div>
+  )
+}
