d1f8ef2ef04d85dc46a11b39b2a7e31f0706723d
Gabriel Díaz <gabrieldiaz31@gmail.com>
2024-08-16 09:23:21 -0300

    fix: Get provider for guest and connected wallets (#467)

--- src/eth/provider.ts
+++ src/eth/provider.ts
@@ -10,9 +10,8 @@ export const SECONDS_IN_MILLIS = 1000
 export const CONNECTION_TIMEOUT_IN_MILLIS = 10 * SECONDS_IN_MILLIS
 
 export const chainIdRpc = new Map<number, string>([
-  [1, 'wss://rpc.decentraland.org/mainnet'],
-  [5, 'wss://rpc.decentraland.org/goerli'],
-  [11155111, 'wss://rpc.decentraland.org/sepolia']
+  [ChainId.ETHEREUM_MAINNET, 'wss://rpc.decentraland.org/mainnet'],
+  [ChainId.ETHEREUM_SEPOLIA, 'wss://rpc.decentraland.org/sepolia']
 ])
 
 export async function getEthereumProvider(
@@ -31,6 +30,10 @@ export async function getEthereumProvider(
       chainId,
       account: null
     }
+  }
+
+  if (connection.isConnected()) {
+    return connection.tryPreviousConnection()
   }
 
   const result = await connection.connect(type, chainId)
